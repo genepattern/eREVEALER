0d846065a70402cacd7742143624c6ba2e79676b
yoshihiko1218 <jim095@ucsd.edu>
2023-12-03 20:46:47 -0600

    Added column information parameter for preprocessing

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ python setup.py sdist bdist_wheel --plat-name=manylinux1_x86_64
 #cp wheelhouse/REVEALER-0.0.*-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl dist
 #cp wheelhouse/REVEALER-0.0.*-cp39-cp39-manylinux*.whl dist
 #rm dist/REVEALER-0.0.*-cp39-cp39-linux_x86_64.whl
-twine upload --repository testpypi dist/*
+#twine upload --repository testpypi dist/*
+twine upload dist/*
